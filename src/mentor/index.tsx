df81343c9195e861ad80b5a006f42a6616ca0453
sarathirangasamy <sarathirangasamy@gmail.com>
2023-07-18 21:47:25 +0530

    mentor page responsive design updated

--- src/mentor/index.tsx
+++ src/mentor/index.tsx
@@ -43,14 +43,14 @@ export const Mentor: React.FC = () => {
                         <Card className="dashboard-cards" hoverable={true}>
                             <Row className="card-content" gutter={16}>
 
-                                <Col xl={5} lg={5} md={5} sm={24} xs={24} style={{ margin: 'auto' }}>
+                                <Col xl={6} lg={6} md={4} sm={24} xs={24} style={{ margin: 'auto' }}>
                                     <div className="">
                                         <img alt='' className='mentor-img-align' src="/images/icon/ellipse.png" />
                                     </div>
                                 </Col>
 
 
-                                <Col xl={15} lg={15} md={15} sm={24} xs={24}>
+                                <Col xl={14} lg={14} md={14} sm={24} xs={24}>
                                     <div className="card-headers">
                                         <h4 className='mentor-custom-style mentor-text-primary'>Sarathi</h4>
                                         <h6 className='mentor-custom-style mentor-text-secondary'>sarathi@blackflux.in</h6>
@@ -88,7 +88,7 @@ export const Mentor: React.FC = () => {
                         <Card className="dashboard-cards" hoverable={true}>
                             <Row className="card-content" gutter={16}>
 
-                                <Col xl={6} lg={6} md={6} sm={24} xs={24} style={{ margin: 'auto' }}>
+                                <Col xl={6} lg={6} md={4} sm={24} xs={24} style={{ margin: 'auto' }}>
                                     <div className="">
                                         <img alt='' className='mentor-img-align' src="/images/icon/ellipse.png" />
                                     </div>
@@ -131,14 +131,14 @@ export const Mentor: React.FC = () => {
                         <Card className="dashboard-cards" hoverable={true}>
                             <Row className="card-content" gutter={16}>
 
-                                <Col xl={5} lg={5} md={5} sm={24} xs={24} style={{ margin: 'auto' }}>
+                                <Col xl={6} lg={6} md={4} sm={24} xs={24} style={{ margin: 'auto' }}>
                                     <div className="">
                                         <img alt='' className='mentor-img-align' src="/images/icon/ellipse.png" />
                                     </div>
                                 </Col>
 
 
-                                <Col xl={15} lg={15} md={15} sm={24} xs={24}>
+                                <Col xl={14} lg={14} md={14} sm={24} xs={24}>
                                     <div className="card-headers">
                                         <h4 className='mentor-custom-style mentor-text-primary'>Sarathi</h4>
                                         <h6 className='mentor-custom-style mentor-text-secondary'>sarathi@blackflux.in</h6>
@@ -176,7 +176,7 @@ export const Mentor: React.FC = () => {
                         <Card className="dashboard-cards" hoverable={true}>
                             <Row className="card-content" gutter={16}>
 
-                                <Col xl={6} lg={6} md={6} sm={24} xs={24} style={{ margin: 'auto' }}>
+                                <Col xl={6} lg={6} md={4} sm={24} xs={24} style={{ margin: 'auto' }}>
                                     <div className="">
                                         <img alt='' className='mentor-img-align' src="/images/icon/ellipse.png" />
                                     </div>
